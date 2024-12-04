8b1bd85a5706bec7c96e6f374f475e23094633cd
AhmedEid2504 <ahmedeid2504@gmail.com>
2024-12-04 16:09:43 +0200

    setup for expo-location

--- App.js
+++ App.js
@@ -3,10 +3,31 @@ import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './src/components/Tabs';
 import Loading from './src/components/Loading';
+import * as Location from 'expo-location';
 
 
 const App= () => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setError('Permission to access location was denied');
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      setLoading(false);
+    })();
+  }, []);
+
+  if (location) {
+    console.log(location);
+  }
+
 
   if (loading) {
     return <Loading />;
