2bb33f2272ce441c7b967aebf5bbde021f325dbb
AhmedEid2504 <ahmedeid2504@gmail.com>
2024-12-04 16:47:06 +0200

    setup for openweatherapi

--- App.js
+++ App.js
@@ -4,12 +4,27 @@ import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './src/components/Tabs';
 import Loading from './src/components/Loading';
 import * as Location from 'expo-location';
-
-
+import { WEATHER_API_KEY } from '@env';
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={c556cca43920bc6c591a5a23a6bb96ce}
 const App= () => {
   const [loading, setLoading] = useState(true);
-  const [location, setLocation] = useState(null);
   const [error, setError] = useState(null);
+  const [weather, setWeather] = useState([]);
+  const [lat, setLat] = useState(null);
+  const [lon, setLon] = useState(null);
+
+  const getWeather = async () => {
+    try {
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+      const data = await response.json();
+      setWeather(data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
 
   useEffect(() => {
     (async () => {
@@ -19,13 +34,18 @@ const App= () => {
         return;
       }
       let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-      setLoading(false);
+      setLon(location.coords.longitude);
+      setLat(location.coords.latitude);
+      if (lat && lon) {
+        await getWeather();
+      }
     })();
-  }, []);
+  }, [lat, lon]);
 
-  if (location) {
-    console.log(location);
+  if (weather) {
+    console.log(lon);
+    console.log(lat);
+    console.log(weather);
   }
 
 
